e47e4a2cba67b3ce9086442158b49cbefff2c745
Noob <ExplodingFKL@gmail.com>
2020-04-19 02:22:49 +0800

    tag 0.1-beta

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    maven
 }
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
--- extras/build.gradle.kts
+++ extras/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    maven
 }
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
