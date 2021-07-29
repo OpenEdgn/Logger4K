f2d147c52c3e44496fe565591484a180a0f72a9e
dragon <ExplodingFKL@gmail.com>
2021-07-29 17:24:12 +0800

    修复日期格式化的线程安全问题

--- logger-core/src/main/kotlin/com/github/openEdgn/logger4k/utils/format/datas/impl/TimeRuleInfo.kt
+++ logger-core/src/main/kotlin/com/github/openEdgn/logger4k/utils/format/datas/impl/TimeRuleInfo.kt
@@ -2,7 +2,10 @@ package com.github.openEdgn.logger4k.utils.format.datas.impl
 
 import com.github.openEdgn.logger4k.utils.format.datas.BaseFormatRule
 import com.github.openEdgn.logger4k.utils.format.datas.IFormatRuleInfo
-import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 /**
  * 日期格式化方案
@@ -15,14 +18,10 @@ class TimeRuleInfo : IFormatRuleInfo<Long> {
     override fun generateRule(rule: String): BaseFormatRule<Long> {
         return object : BaseFormatRule<Long>(rule) {
             override val name: String = this@TimeRuleInfo.name
-            private val dateFormat = object : ThreadLocal<SimpleDateFormat>() {
-                override fun initialValue(): SimpleDateFormat {
-                    return SimpleDateFormat(rule)
-                }
-            }
-
+            private val dateFormat = DateTimeFormatter.ofPattern(rule)
+            private val systemDefault = ZoneId.systemDefault()
             override fun format(data: Long): String {
-                return dateFormat.get().format(data)
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(data), systemDefault).format(dateFormat)
             }
         }
     }
